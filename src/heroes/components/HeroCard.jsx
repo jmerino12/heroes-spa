86912bcd18da1f07fe225dadc04a689b9efb9f38
Jonathan Meriño Bolivar <jmerino1204@gmail.com>
2022-08-04 10:23:06 -0500

    Tarjetas con la información del Héroe

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -5,7 +5,23 @@ export const HeroCard = ({
     alter_ego,
     first_appearance,
     characters }) => {
+    const heroImageUrl = `/assets/heroes/${id}.jpg`
     return (
-        <div>HeroCard</div>
-    )
+        <div className="col">
+            <div className="card">
+                <div className="row no-gutters">
+                    <div className="col-4">
+                        <img src={ heroImageUrl } className="card-img" alt={superhero} />
+                    </div>
+                    <div className="col-8">
+                        <div className="card-body">
+                            <h5 className="card-title">{superhero}</h5>
+                            <p className="card-text">{alter_ego}</p>
+                            <p>{characters}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
