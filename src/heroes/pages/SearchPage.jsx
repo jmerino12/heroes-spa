f37f03ae71f2948c91da1ac7bfbfa12cb8581717
Jonathan Meriño Bolivar <jmerino1204@gmail.com>
2022-08-05 21:26:25 -0500

    Mostrar mensajes condicionales

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -7,17 +7,19 @@ import { useMemo } from "react";
 
 export const SearchPage = () => {
   const navigate = useNavigate();
-  const {q = ''} = queryString.parse(location.search);
-  const heroes = useMemo( () => getHeroesByName(q), [q]);
-  
-  const {searchText, onInputChange} = useForm({
+  const { q = '' } = queryString.parse(location.search);
+  const heroes = useMemo(() => getHeroesByName(q), [q]);
+  const showSearch = (q.length === 0);
+  const showError = (q.length > 0) && heroes.length === 0;
+
+  const { searchText, onInputChange } = useForm({
     searchText: q
   });
 
 
   const onSearchSubmit = (event) => {
     event.preventDefault();
-    if(searchText.trim().length <= 1) return;
+    //if (searchText.trim().length <= 1) return;
     navigate(`?q=${searchText.toLowerCase().trim()}`);
   }
   return (
@@ -49,14 +51,18 @@ export const SearchPage = () => {
         <div className="col-7">
           <h4>Results</h4>
           <hr />
-          <div className="alert alert-primary">
+          {/*q === '' ? <div className="alert alert-primary">
+            Search a hero
+          </div> : (heroes.length === 0) && <div className="alert alert-danger">
+            No hero with  <b>{q}</b>
+  </div>*/}
+          <div className="alert alert-primary animate__animated animate__fadeIn" style={{ display: showSearch ? '' : 'none' }}>
             Search a hero
           </div>
-          <div className="alert alert-danger">
+
+          <div className="alert alert-danger animate__animated animate__fadeIn" style={{ display: showError ? '' : 'none' }}>
             No hero with  <b>{q}</b>
           </div>
-
-
           {
             heroes.map(hero => (
               <HeroCard key={hero.id} {...hero} />
