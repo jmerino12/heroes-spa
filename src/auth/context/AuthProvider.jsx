22889ebe1e147c234e148954747aec663764aedc
Jonathan Meriño Bolivar <jmerino1204@gmail.com>
2022-08-06 15:57:34 -0500

    Login de un usuario

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -1,13 +1,29 @@
 import React, { useReducer } from 'react'
 import { AuthContext } from './';
 import { authReducer } from './authReducer';
+import { types } from '../types/types';
+
 
 export const AuthProvider = ({ children }) => {
 
-    const [state, dispatch] = useReducer(authReducer, { logged:false });
+    const [state, dispatch] = useReducer(authReducer, { logged: false });
+
+    const login = (name = '') => {
+        const action = {
+            type: types.login,
+            payload: {
+                id: 'ABD',
+                name
+            }
+        }
+        dispatch(action);
+    }
 
     return (
-        <AuthContext.Provider value={{}}>
+        <AuthContext.Provider value={{
+            login,
+            ...state
+        }}>
             {children}
         </AuthContext.Provider>
     );
