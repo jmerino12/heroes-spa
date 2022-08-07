30745b3ab2969ccb089448b61a5c03d24339e2e2
Jonathan Meriño Bolivar <jmerino1204@gmail.com>
2022-08-07 16:53:35 -0500

    Mantener el usuario activo

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -3,19 +3,30 @@ import { AuthContext } from './';
 import { authReducer } from './authReducer';
 import { types } from '../types/types';
 
+const init = () => {
+    const user = JSON.parse(localStorage.getItem('user'));
+    return {
+        logged: !!user,
+        user
+    }
+}
+
 
 export const AuthProvider = ({ children }) => {
 
-    const [state, dispatch] = useReducer(authReducer, { logged: false });
+    const [state, dispatch] = useReducer(authReducer, { }, init);
 
     const login = (name = '') => {
+        const user =  {
+            id: 'ABD',
+            name
+        }
         const action = {
             type: types.login,
-            payload: {
-                id: 'ABD',
-                name
-            }
+            payload: user
+
         }
+        localStorage.setItem('user', JSON.stringify(user));
         dispatch(action);
     }
 
