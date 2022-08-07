a05c8535c3861f9194c1a4b6f06c3ff3afa41f84
Jonathan Meriño Bolivar <jmerino1204@gmail.com>
2022-08-07 17:09:08 -0500

    Logout del usuario

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -29,10 +29,18 @@ export const AuthProvider = ({ children }) => {
         localStorage.setItem('user', JSON.stringify(user));
         dispatch(action);
     }
+    const logout = () => {
+        localStorage.removeItem('user');
+        const action = {
+            type: types.logout,
+        }
+        dispatch(action);
+    }
 
     return (
         <AuthContext.Provider value={{
             login,
+            logout,
             ...state
         }}>
             {children}
