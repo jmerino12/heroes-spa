5ce7f0ee8adf75ebe1a621d3edaa4c249e0057ea
Jonathan Meriño Bolivar <jmerino1204@gmail.com>
2022-08-07 17:18:45 -0500

    Rutas privadas

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,13 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
 import { LoginPage } from '../auth/pages/LoginPage';
 import { HeroesRoutes } from '../heroes/routes/HeroesRoutes';
+import { PrivateRoute } from './PrivateRoute';
 
 export const AppRouter = () => {
     return (
         <>
             <Routes>
                 <Route path='login' element={<LoginPage />} />
-                <Route path='/*' element={<HeroesRoutes />} />
+                <Route path='/*' element={
+                    <PrivateRoute>
+                        <HeroesRoutes />
+                    </PrivateRoute>
+                }>
+                </Route>
             </Routes>
         </>
     );
