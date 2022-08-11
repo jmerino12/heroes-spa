e123d487c78096bbb494c38ea140ffc84d642c96
Jonathan Meriño Bolivar <jmerino1204@gmail.com>
2022-08-11 08:58:20 -0500

    Pruebas en PrivateRouter

---
+++ test/router/PrivateRouter.test.jsx
@@ -0,0 +1,29 @@
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import { AuthContext } from "../../src/auth/context";
+import { PrivateRoute } from "../../src/router/PrivateRoute";
+
+describe('Pruebas en el PrivateRoute', () => {
+    test('Debe de mostrar el children si esta auth', () => {
+        Storage.prototype.setItem = jest.fn();
+        const contextValue = {
+            logged: true,
+            user: {
+                name: 'Jonathan',
+                id: '123'
+            }
+        }
+        render(
+            <AuthContext.Provider value={contextValue}>
+                <MemoryRouter initialEntries={['/search?q=batman']}>
+                    <PrivateRoute>
+                        <h1>Ruta privada</h1>
+                    </PrivateRoute>
+                </MemoryRouter>
+
+            </AuthContext.Provider>
+        );
+        expect(screen.getByText("Ruta privada")).toBeTruthy();
+        expect(localStorage.setItem).toHaveBeenCalledWith("lastPath", "/search?q=batman");
+    });
+});
